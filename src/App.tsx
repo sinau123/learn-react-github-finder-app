aba4a54bbb9a7ab3551d43db0797596e8534a654
Denis Eko Harbiyanto <sinau123@gmail.com>
2022-01-10 05:05:36 +0700

    [ADD] tailwind & daisy UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-[120px]">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
