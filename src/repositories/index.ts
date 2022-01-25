c583c01e67be9d3c83cb94031e1f4cbe1bfa25f7
Denis Eko Harbiyanto <sinau123@gmail.com>
2022-01-25 14:31:33 +0700

    [FIX] eslint

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,9 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_GITHUB_API_URL
-})
+  baseURL: process.env.REACT_APP_GITHUB_API_URL,
+});
 
-api.defaults.headers.common['Authorization'] = `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+api.defaults.headers.common[
+  "Authorization"
+] = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
 
-export default api
+export default api;
