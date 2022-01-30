8c62632c762cc76354015c1e32dea633f784df30
Denis Eko Harbiyanto <sinau123@gmail.com>
2022-01-30 11:54:39 +0700

    [FIX] API

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -4,8 +4,10 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_GITHUB_API_URL,
 });
 
-api.defaults.headers.common[
-  "Authorization"
-] = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
+if (process.env.REACT_APP_GITHUB_TOKEN) {
+  api.defaults.headers.common[
+    "Authorization"
+  ] = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
+}
 
 export default api;
