03d625964c0c6273204b14df32867738dfac94a1
Denis Eko Harbiyanto <sinau123@gmail.com>
2022-01-30 11:55:50 +0700

    [ADD] github page & hooks eslint

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,20 @@ import User from "./pages/User";
 function App() {
   return (
     <GithubProvider>
-      <Router>
+      <Router
+        basename={
+          process.env.NODE_ENV === "development"
+            ? undefined
+            : process.env.PUBLIC_URL
+        }
+      >
         <div className="flex flex-col justify-between h-screen">
           <Navbar />
           <main className="container mx-auto px-3 flex-1">
             <Routes>
               <Route path="/" element={<Home />}></Route>
               <Route path="/about" element={<About />}></Route>
-              <Route path="/user/:login" element={<User />}></Route>
+              <Route path="/user/:username" element={<User />}></Route>
               <Route path="/not-found" element={<NotFound />}></Route>
               <Route path="/*" element={<NotFound />}></Route>
             </Routes>
